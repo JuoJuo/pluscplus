b923bec5c862fe3097b4c473b4888b079403ee41
juojuo <01lirenjie@gmail.com>
2022-03-22 08:55:54 +0800

    vertex shader and fragment shader

--- opengllearn/Application.cpp
+++ opengllearn/Application.cpp
@@ -3,6 +3,56 @@
 #include <GLFW/glfw3.h>
 
 /*
+  vertex shader 有多少个vertex就调用多少次。
+  fragment shader 目前可以理解为有多少个px就调用多少次。
+
+*/
+static unsigned int CompileShader(unsigned int type, const std::string& souce) {
+  unsigned int programID = glCreateShader(type);
+  const char* src = souce.c_str();
+  glShaderSource(programID, 1, &src, nullptr);
+  glCompileShader(programID);
+
+
+  int result;
+
+  glGetShaderiv(programID, GL_COMPILE_STATUS, &result);
+  if (result == GL_FALSE) {
+    int length;
+    glGetShaderiv(programID, GL_INFO_LOG_LENGTH, &length);
+
+    char* message = (char*)_alloca(length * sizeof(char));
+
+    glGetShaderInfoLog(programID, length, &length, message);
+
+    std::cout << "Failed to compile " << (type == GL_VERTEX_ARRAY ? "vertex" : "fragment") << " shader" << std::endl;
+    std::cout << message << std::endl;
+
+    glDeleteShader(programID);
+
+    return 0;
+  }
+
+  return programID;
+}
+
+static int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
+  unsigned int program = glCreateProgram();
+  unsigned int vsid = CompileShader(GL_VERTEX_SHADER, vertexShader);
+  unsigned int fsid = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+  glAttachShader(program, vsid);
+  glAttachShader(program, fsid);
+  glLinkProgram(program);
+  glValidateProgram(program);
+
+  glDeleteShader(vsid);
+  glDeleteShader(fsid);
+
+  return program;
+}
+
+/*
 
  opengl是个状态机，简单讲，工作模式就是，我指定一个buffer，指定一个shader，然后告诉你画某个图形。就会绘制了
 
@@ -64,6 +114,46 @@ int main(void)
   */
   glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
+
+  /*
+    330 gl shader language 330版本
+
+    core不允许使用过期函数
+
+    layout(location=0)是glVertexAttribPointer第一个参数。表示从第几个点开始接收
+
+    gl_Position是固定的变量名字，接收vec4类型.
+
+    我们注意到此处我们是vec2，赋值的时候还得调用具体的函数转换，所以就直接定义为vec4了
+  */
+
+  std::string vertexShader =
+    "#version 330 core\n"
+    "\n"
+    "layout(location = 0) in vec4 position;\n"
+    "\n"
+    "void main()\n"
+    "{\n"
+    " gl_Position = position;\n"
+    "}\n";
+
+
+  /*
+
+    color rgba  0-255 映射为  0-1
+  */
+  std::string fragmentShader =
+    "#version 330 core\n"
+    "\n"
+    "layout(location = 0) out vec4 color;\n"
+    "\n"
+    "void main()\n"
+    "{\n"
+    " color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+    "}\n";
+
+  unsigned int id_program = CreateShader(vertexShader, fragmentShader);
+  glUseProgram(id_program);
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window))
   {
@@ -83,7 +173,7 @@ int main(void)
     /* Poll for and process events */
     glfwPollEvents();
   }
-
+  glDeleteShader(id_program);
   glfwTerminate();
   return 0;
 }
